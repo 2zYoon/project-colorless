c21ecc0e2f63f65afabc97be89d2fb011d3a1cc6
EunSeong-Park <49016892+EunSeong-Park@users.noreply.github.com>
2022-08-21 23:17:56 +0900

    minor change

--- _lib/constants.py
+++ _lib/constants.py
@@ -1,23 +1,21 @@
-# Several constants
-from typing import Final
 
 ##########
 # Common #
 ##########
 
 # Colors
-WHITE: Final =      (255, 255, 255)
-BLACK: Final =      (0, 0, 0)
-GRAY: Final =       (128, 128, 128)
-GRAY10: Final =     (26, 26, 26)
-GRAY20: Final =     (51, 51, 51)
-GRAY30: Final =     (76, 76, 76)
-GRAY40: Final =     (102, 102, 102)
-GRAY50: Final =     (128, 128, 128)
-GRAY60: Final =     (153, 153, 153)
-GRAY70: Final =     (179, 179, 179)
-GRAY80: Final =     (204, 204, 204)
-GRAY90: Final =     (230, 230, 230)
+WHITE =      (255, 255, 255)
+BLACK =      (0, 0, 0)
+GRAY =       (128, 128, 128)
+GRAY10 =     (26, 26, 26)
+GRAY20 =     (51, 51, 51)
+GRAY30 =     (76, 76, 76)
+GRAY40 =     (102, 102, 102)
+GRAY50 =     (128, 128, 128)
+GRAY60 =     (153, 153, 153)
+GRAY70 =     (179, 179, 179)
+GRAY80 =     (204, 204, 204)
+GRAY90 =     (230, 230, 230)
 
 
 ############
@@ -26,11 +24,11 @@ GRAY90: Final =     (230, 230, 230)
 
 # Layouts
 # key: (val-small, val-medium, val-large)
-LAYOUT_SMALL: Final = 0
-LAYOUT_MEDIUM: Final = 1
-LAYOUT_LARGE: Final = 2
+LAYOUT_SMALL = 0
+LAYOUT_MEDIUM = 1
+LAYOUT_LARGE = 2
 
-LAUNCHER_LAYOUT: Final = {
+LAUNCHER_LAYOUT = {
     # Window size: (w, h)
     "window": [ 
         (400, 300), 
@@ -39,20 +37,20 @@ LAUNCHER_LAYOUT: Final = {
     ],
     # Game start button: (w, h, x, y)
     "btn_gamestart": [
-        (280, 90, 10, 200, 30),
-        (460, 100, 50, 460, 36),
-        (740, 150, 60, 700, 48)
+        (280, 40, 10, 250, 30),
+        (460, 50, 50, 510, 36),
+        (740, 100, 60, 750, 48)
     ],
     # Setting button: (w, h, x, y, font-size)
     "btn_setting": [
-        (90, 90, 300, 200, 22),
-        (200, 100, 540, 460, 28),
-        (300, 150, 840, 700, 40)
+        (90, 40, 300, 250, 22),
+        (200, 50, 540, 510, 28),
+        (300, 100, 840, 750, 40)
     ],
 }
 
 # Stylesheets
-SS_BUTTON_DEFAULT: Final = """
+SS_BUTTON_DEFAULT = """
 QPushButton { 
     background-color: #202020; 
     border: none; 
@@ -64,7 +62,7 @@ QPushButton:hover {
     background-color: #303030; 
 }"""
 
-SS_BUTTON_CTRL_DEFAULT: Final = """
+SS_BUTTON_CTRL_DEFAULT = """
 QPushButton { 
     border: none; 
     font: bold; 
@@ -74,7 +72,7 @@ QPushButton:hover {
     background-color: #303030; 
 }"""
 
-SS_BUTTON_CTRL_DISABLED: Final = """
+SS_BUTTON_CTRL_DISABLED = """
 QPushButton { 
     border: none; 
     font: bold; 
@@ -82,9 +80,9 @@ QPushButton {
     color: gray; 
 }"""
 
-SS_FONTSIZE: Final = ["QPushButton { font-size: %dpx; }" % i for i in range(100) ]
+SS_FONTSIZE = ["QPushButton { font-size: %dpx; }" % i for i in range(100) ]
 
 ###############
 # Game engine #
 ###############
-FPS: Final = 60
+FPS = 60
