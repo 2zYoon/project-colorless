e76b5da489801b8d4664f325e36c5a736a1f2605
2zyoon <dmstjd517@unist.ac.kr>
2022-08-27 18:01:43 +0900

    more update

--- _lib/map.py
+++ _lib/map.py
@@ -10,15 +10,18 @@ class MAP:
     def __init__(self, size):
         # Meta: Overall metadata
         # [0-1]
-        #   00: Use terrain code
-        #   01: Fill with BLACK
-        #   10: Make Transparent
-        #   11: UNUSED
+        #   00: Use LAYER 0
+        #   01: Use LAYER 1
+        #   10: Make transparent
+        #   11: Fill with BLACK
         # []
         self.meta = np.zeros(size, dtype="uint16")
         
-        # Terrain: Code for terrain visualization
-        self.terrain = np.zeros(size, dtype="uint16")
+        # Layer1: Mostly used. This is under the character
+        self.layer1 = np.zeros(size, dtype="uint16")
+
+        # Layer2: This is on the chracter
+        self.layer2 = np.zeros(size, dtype="uint16")
 
         # System: Used in game
         self.system = np.zeros(size, dtype="uint16")
