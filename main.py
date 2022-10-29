b2cdb6ea95cbadca668ee71ad2a64c9b8c2c92d0
EunSeong-Park <49016892+EunSeong-Park@users.noreply.github.com>
2022-10-30 05:54:50 +0900

    make monotonic

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from ursina import *
 from ursina.prefabs.platformer_controller_2d import PlatformerController2d
 
+from PIL import Image
+
+TRANSPARENT =   (0, 0, 0, 0)
+WHITE =         (255, 255, 255, 255)
+BLACK =         (0, 0, 0, 255)
+GRAY =          (128, 128, 128, 255)
+
+ALPHA = lambda pct: int(255 * pct / 100)
+
+# PRELOAD
+#preload_block_1 = Entity(model="cube", texture="asset/block/1.png", scale=(1,1), origin=(-.5, .5))
+
 def toggle_button_list(button_list):
     for button in button_list:
         button.enabled = not button.enabled
@@ -16,8 +28,9 @@ def render_map(mapname="test"):
             col = texture.get_pixel(x, y)
             
             if col == color.black:
-                Entity(model="cube", color=color.white33, origin=(-.5, .5), scale=(1, 1), x=x, y=y, collider="box")
-            
+                mod = (x + y) % 5
+                Entity(model="cube", texture=f"asset/block/{mod}.png", origin=(-.5, .5), scale=(1, 1), x=x, y=y, collider="box")
+                    
 
     # we cannot go further
     Entity(model="cube", color=(255, 0, 0, 255), origin=(-.5, .5), scale=(0.2, texture.height), x=0, y=(texture.height-1), collider="box")
@@ -26,8 +39,10 @@ def render_map(mapname="test"):
 
 
 app = Ursina(editor_ui_enabled=False)
+app.setBackgroundColor(255,255,255)
+
 camera.orthographic = True
-camera.fov = 20
+camera.fov = 30
 
 button_list_menu = [
     Button(text="Option", scale_x=0.2, scale_y=0.05, text_origin=(0,0), position= (0.5*window.aspect_ratio - 0.1, 0.5 - 0.075), on_click=lambda: application.quit()),
@@ -60,14 +75,14 @@ def input(key):
 render_map()
 
 player = PlatformerController2d(
-    scale_y=1,
+    scale_y=1.5,
     jump_height=4,
     x=3, 
-    y=5,
-    jump_duration = 0.3,
+    y=6,
+    jump_duration = 0.5,
     use_jump_dust=False)
 
-camera.add_script(SmoothFollow(target=player, offset=[0,1,-30], speed=2))
+camera.add_script(SmoothFollow(target=player, offset=[0,5,-30], speed=2))
 #EditorCamera()
 
 
